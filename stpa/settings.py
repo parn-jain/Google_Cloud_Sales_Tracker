a2236f4f1aa4b506ad761b35b1e5c406c1cab47e
“parn” <pjisvgreat@gmail.com>
2024-09-13 12:10:36 +0530

    settings change

--- stpa/settings.py
+++ stpa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_zn&3l08*xa0#$*v7l8y#h^v1)7#w$&_c)s=+z$(f7v8f3hhu=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.100.231.191']
 
 
 # Application definition
@@ -184,6 +184,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOTS = os.path.join(BASE_DIR,'static/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
